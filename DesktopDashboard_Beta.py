6cee07c9bae568fc0f74fade9e2487fdc7dc9c96
ruben <rubenski0345@gmail.com>
2020-03-21 17:46:52 +0100

    Has a visible GUI with used storage on a bar

--- DesktopDashboard_Beta.py
+++ DesktopDashboard_Beta.py
@@ -9,9 +9,13 @@ import turtle
 
 def Gui(percentage, naamschijf):
   root = Tk()
-  root.geometry('300x300')
 
-  c = Canvas(root, height=250, width=300, bg="white")
+  root.geometry('300x300')
+  root.overrideredirect(True)
+  root.overrideredirect(False)
+
+  c = Canvas(root, height=250, width=300, bg="white", bd=0 ,highlightthickness=0, )
+
 
   originelheight = 100
   originelwidth = 250
@@ -20,7 +24,16 @@ def Gui(percentage, naamschijf):
 
   lbl = Label(root, text=naamschijf + " "+ str( (int(percentage * 100))) + "% used")
 
+  def remove_func():
+      emptyMenu = Menu(root)
+      root.config(menu=emptyMenu)
+
+
+
   lbl.grid(column=0, row=1)
+
+  remove_button = Button(root, text="Remove", command=remove_func)
+  remove_button.grid(column=0, row=3)
   # x1, (l) y1,(u) x2, (r) y2 (d)
   rect = c.create_rectangle(10, 50, originelwidth, originelheight, fill='black')
   fillrect = c.create_rectangle(10, 50, perwidth, perheight, fill='red')
